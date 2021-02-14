abad6704c684039df18b1de84fa0a24777bd0d30
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-14 15:38:20 -0500

    pseudo code questions function and copied requirements into readme

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const fs = require("fs");
 const path = require("path")
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  //question for title
+
+  // Description of project
+  // Table of Contents, which will include:
+
+  // Installation
+  // Usage - include website link
+  // license: MIT, Apache 2.0, GPL 3.0 BSD3, none
+  // Contributing
+  // Tests
+  // Questions - include contact information and github link
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
