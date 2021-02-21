99b08c01cbd2687fc091a5addf9386a6a8f2e684
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-21 11:59:51 -0500

    updates

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -18,7 +18,7 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if(license !== "None") {
-    return `##License
+    return `##  License
 
 The license for this project is ${license}. For full details, refer to the license file within the repository.`
   }
