0fa97e160b1246f61b776aa222e4c0119fc5b89d
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-02-20 20:29:54 -0500

    working on template literal

--- index.js
+++ index.js
@@ -1,101 +1,73 @@
-// TODO: Include packages needed for this application
-const inquirer = require("inquirer");
-const fs = require("fs");
-const path = require("path")
-const generateMarkdown = require("./utils/generateMarkdown")
+// Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-// TODO: Create an array of questions for user input
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'contactInfo',
-      message: 'Enter your contact information: (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your contact information');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your GitHub username');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'title',
-      message: 'Enter your project name (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your project name');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'repoName',
-      message: 'Enter the repository name: (Required)',
-      validate: nameInput => {
-        if (nameInput) {
-          return true;
-        } else {
-          console.log('Please enter your repository name');
-          return false;
-        }
-      }
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Add a description for your project'
-    },
-    {
-      type: 'checkbox',
-      name: 'license',
-      message: 'Select a license (choose one):',
-      choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD3', 'None'],
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: '',
-    },
-    {
+// Create an array of questions for user input
+const questions = [
+  {
+    type: 'input',
+    name: 'contactInfo',
+    message: 'Enter your contact email: (Required)'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username (Required)'
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: 'Enter your project name (Required)',
+  },
+  {
+    type: 'input',
+    name: 'repoName',
+    message: 'Enter the repository name: (Required)'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Add a description for your project'
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'Select a license (choose one):',
+    choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD3', 'None']
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'What does a user need to know about the project?',
+  },
+  {
       type: 'input',
       name: 'install',
       message: 'Are there any installation requirements?'
-    },
-    {
-      type: 'input',
-      name: 'test',
-      message: '',
-    },
-  ]);
-};
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'Are there any tests to be run?',
+  },
+  {
+    type: 'input',
+    name: 'contribute',
+    message: 'How to contribute to the project?',
+  },
+];
 
-// TODO: Create a function to write README file
+// Create a function to write README file
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
-// TODO: Create a function to initialize app
+// Create a function to initialize app
 function init() {
-  inquirer.prompt(questions).then(answers => {writeToFile("${data.title}-README.md", generateMarkdown(answers))})
+  return inquirer.prompt(questions)
+    .then(answers => {writeToFile("$generatedREADME.md", generateMarkdown(answers))})
 }
 
 // Function call to initialize app
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,41 +1,88 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  if(license !== "none") {
-    return ``
+  if(license !== "None") {
+    return `![GitHub license badge](https://img.shields.io/badge/license-${license}-orange)`
   }
+  return ''
 }
 
-// TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if(license !== "none") {
-    return ``
+  if(license !== "None") {
+    return `[License](#License)`
   }
+  return ''
 }
 
-// TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if(license !== "none") {
-    return ``
+  if(license !== "None") {
+    return `##License
+
+The license for this project is ${license}. For full details, refer to the license file within the repository.`
   }
+  return ''
 }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  console.log(data);
   return `# ${data.title}
 
-  //Title
-  // Description of project
-  // Table of Contents, which will include:
+## Description
+${data.description}
 
-  // Installation
-  // Usage - include website link
-  // license: MIT, Apache 2.0, GPL 3.0 BSD3, none
-  // Contributing
-  // Tests
-  // Questions - include contact information and github link
+${renderLicenseBadge(data.license)}
+
+Table of Contents |
+-------------------|
+[Installation](#Installation)
+[Usage](#Usage)
+[Contributing](#Contributing)
+${renderLicenseLink(data.license)}
+[Tests](#Tests)
+[Questions](#Questions)
+|
+
+<br />
+
+## Installation
+
+In order to run the project locally, ensure that dependencies are installed by running:
+
+${data.install}
+
+<br />
+
+## Usage
+
+${data.usage}
+
+You can find the deployed link and the repository link below:
+
+Deployed Link: [Deployed Link](https://${data.github}.github.io/${data.repoName})
+Repo Link: [Repo Link](https://github.com/${data.github}/${data.repoName})
+
+<br />
+
+## Contributing
+
+${data.contribute}
+
+<br />
+
+${renderLicenseSection(data.license)}
+
+## Tests
+
+${data.test}
+
+<br />
+
+## Questions
+
+If you have any questions or issues, feel free to reach out through ${data.contactInfo}.
+You can also find more of my work on Github at [github link](https://github.com/${data.github})
 `;
 }
 
